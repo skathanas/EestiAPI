2a87926848b9d1a6535ab284ea37f633e365ef9e
skathanas <32923297+skathanas@users.noreply.github.com>
2020-01-24 08:27:58 +0200

    Add files via upload

---
+++ EestiAPI/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy.Json;
+using Newtonsoft.Json;
+
+namespace EestiAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to facts about Estonia!");
+            EstFacts();
+        }
+
+        public static void EstFacts()
+        {
+            string EstFactsUrl = "https://restcountries.eu/rest/v2/name/eesti";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(EstFactsUrl);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                info eeInfo = JsonConvert.DeserializeObject<info>(response);
+                Console.WriteLine(eeInfo.name);
+                Console.WriteLine(eeInfo.cioc);
+                Console.WriteLine(eeInfo.domain);
+                Console.WriteLine(eeInfo.capital);
+                Console.WriteLine(eeInfo.region);
+                Console.WriteLine(eeInfo.population);
+                Console.WriteLine(eeInfo.language);
+            }
+        }
+
+    }
+}
---
+++ EestiAPI/info.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy.Json;
+
+namespace EestiAPI
+{
+    class info
+    {
+        public string name { get; set; }
+        public string cioc { get; set; }
+        public string domain { get; set; }
+        public string capital { get; set; }
+        public string region { get; set; }
+        public string population { get; set; }
+        public string language { get; set; }
+    }
+}
